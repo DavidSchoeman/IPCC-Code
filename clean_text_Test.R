030632a074e8443c3495eecb1d7131c02ad1a983
DavidSchoeman <36880833+DavidSchoeman@users.noreply.github.com>
2021-03-18 11:56:51 +1000

    Explain search terms

--- clean_text_Test.R
+++ clean_text_Test.R
@@ -1,65 +1,11 @@
+# Using Mac Preview, search on "ocea | coast | marin | fish | coral | reef | plank"
+
 h <- "Document for Govt and Expert Review
 4a620740
 FIRST ORDER DRAFT Summary for Policymakers IPCC WGII Sixth Assessment Report"
 f <- "Do Not Cite, Quote or Distribute SPM-7 Total pages: 57"
-
-x <- "
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21 B.2.1 Anthropogenic climate change has exposed ocean and coastal ecosystems to conditions unprecedented
-22 over centuries to millennia (very high confidence). Warming is causing shifts in species abundance and
-23 community composition, from plankton to fish as well as shifts in distribution by latitude (30 to 55 km per
-24 decade since pre-industrial) and depth, and changes in seasonal timing (from 4.5 to 6.0 days per decade
-25 earlier), in every ocean region (high to very high confidence). These responses have caused habitat loss,
-26 population declines, increased risks of local extirpations and reorganisation of marine food webs. (medium to
-27 high confidence). {3.2.2, 3.2.3, 3.4.2, 3.4.3, 3.4.4, 11.3.2, Table 11-6, 13.1, 15.3, 16.x, CCP1.2.1, CCP4.1.4,
-28 Cross-Chapter Box MOVING SPECIES in Chapter 5, Cross-Chapter Box ILLNESS in Chapter 2, SROCC}.
-29
-30 B.2.2 A wide range of marine ecosystems have been impacted by extreme events, superimposed on long-
-31 term climate-driven changes (very high confidence). Widespread impacts caused by marine heatwaves in
-32 coastal systems include extensive coral bleaching and mortality, other mass mortalities, decline of kelp
-33 forests, loss or restructuring of seagrass-meadow habitat, arrival of new species (including invasive species),
-34 and shifts in community structure across many marine systems. (very high confidence). {3.4.2, 5.9.3, 11.3.2,
-35 16.x, Cross-Chapter Box EXTREMES in Chapter 2}
-36
-37 B.2.3 Climate warming exacerbates the ecological consequences of land-based nutrient pollution and
-38 formation of low-oxygen (dead) zones in coastal areas (medium confidence). Changes in sediment supply
-39 and nearby urban and industrial development enhance vulnerability of sandy beaches, saltmarshes and
-40 mangrove forests to sea-level rise (very high confidence). Mangroves continue to face multiple compounding
-41 threats due to pollution, and conversion for aquaculture, agriculture or coastal infrastructure, in addition to
-42 climate-based sea-level rise and coastal erosion. [confidence]. {3.3.3, 3.4.2, 5.9.3, 12.x}
-43
-44 B.2.4 Climate-driven changes to polar and sub-polar ecosystems are unprecedented, based on observations
-45 from multiple knowledge systems. Persistent warming, combined with heatwaves, and loss of permafrost,
-46 sea ice, and snow has contributed to cascading environmental and, in the Arctic, socio-cultural impacts,
-47 including conversion of Arctic into boreal ecosystems, abrupt redistribution of species, and synchronous and
-48 widespread bird, whale, and fish mortality. (high to very high confidence). {3.4.2, 5.7.3, 14.4.2, 14.4.4,
-49 14.4.6, 14.4.7, 14.4.8, 14.4.11, Box 14.6, CCP6.2.1, CCP6.2.4, CCP6.2.5, CCP6.2.6, Cross-Chapter Box
-50 MOVING SPECIES in Chapter 5}
-51
-52 B.2.5 Climate change has increased the extent and frequency of water-related diseases and harmful algal
-53 blooms. They are threatening not only human and ecosystem health but also food security and the economy
-54 and livelihoods of many coastal communities. (high confidence) {3.5, Box 3.3, 5.8.3, 5.9.4, 5.11.1, 5.12.3,
-55 14.4.6, CCP6.2.8, Cross-Chapter Box ILLNESS in Chapter 2}
-56
- 
-B.2 Ocean warming, with increases in the frequency and intensity of short-term marine heatwaves, ocean acidification, stratification, and deoxygenation are negatively impacting marine ecosystems and reducing fisheries productivity (high confidence). Findings are in line with the paleo-record which shows how global warming caused species to migrate poleward, how sea-level rise drowned coastal ecosystems, and how extreme warming and ocean deoxygenation caused mass extinctions. Local and regional non-climate hazards [examples] continue to worsen, and to be worsened by, observed impacts of climate change on marine life (very high confidence). {3.2, 3.4.2, 3.4.4, Box 3.1, 5.8.1, 5.9.1, Cross-Chapter Box MOVING SPECIES in Chapter 5, Cross-Chapter Box ILLNESS in Chapter 2, Cross-Chapter Box EXTREMES in Chapter 2, Figure SPM.1, Figure SPM.2}.
- Do Not Cite, Quote or Distribute SPM-7 Total pages: 57
-Document for Govt and Expert Review
-4a620740
-FIRST ORDER DRAFT Summary for Policymakers IPCC WGII Sixth Assessment Report
-1 B.2.6 Adaptation limits have already been reached by algae/seaweeds, wetland vegetation, cnidarians/corals,
-2 marine fish, molluscs, crustaceans and marine mammals, impacting ecosystems especially at geographic
-3 boundaries (medium confidence). Only a small set of adaptation measures directly intended to sustain
-4 ecosystem services in ocean and coastal systems have been implemented at effective scales (high
-5 confidence). {3.4.2, 3.6.3, 13.4.2, 16.3.1, 16.4.2, 16.4.3}"
+x <- ""
+x <- "B.2 Ocean warming, with increases in the frequency and intensity of short-term marine heatwaves, ocean acidification, stratification, and deoxygenation are negatively impacting marine ecosystems and reducing fisheries productivity (high confidence). Findings are in line with the paleo-record which shows how global warming caused species to migrate poleward, how sea-level rise drowned coastal ecosystems, and how extreme warming and ocean deoxygenation caused mass extinctions. Local and regional non-climate hazards [examples] continue to worsen, and to be worsened by, observed impacts of climate change on marine life (very high confidence). {3.2, 3.4.2, 3.4.4, Box 3.1, 5.8.1, 5.9.1, Cross-Chapter Box MOVING SPECIES in Chapter 5, Cross-Chapter Box ILLNESS in Chapter 2, Cross-Chapter Box EXTREMES in Chapter 2, Figure SPM.1, Figure SPM.2}."
 
 source("IPCC_Helpers.R")
 
